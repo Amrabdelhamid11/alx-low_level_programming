12f51001b5c6f765b6fee1dabb039b730a9e008b
Amr <amrmedo756@gmail.com>
2023-10-08 20:07:52 -0700

    last

---
+++ math.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * Description : heree
+*/
+int main(void)
+{
+	int x = 3;
+	int y = 4;
+
+	printf(" x + y = %d\n", x + y);
+	printf(" x - y = %d\n", x - y);
+	printf(" x * y = %d\n", x * y);
+	printf(" x / y = %d\n", x / y);
+	return (0);
+}
