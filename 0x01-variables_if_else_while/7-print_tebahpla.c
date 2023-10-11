cedc9948df3f4f67e1ff7a0656905eede60e3b91
Amrabdelhamid11 <amrmedo756@gmail.com>
2023-10-11 14:46:36 -0700

    aaaaaaaaa

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -3,16 +3,16 @@
  * main - Entry point
  * Return: Always 0 (Success)
  * --------------------------
- * Description :Write a program that prints all single reverse  alpha  by a new line.
- * --------------------------
- */
+ * Description :Write a single reverse  alpha  by a new line.
+ * -------------------------
+*/
 int main(void)
 {
 	char chart;
 
 	for (chart = 'z'; chart >= 'a'; chart--)
 	{
-		putchar( chart);
+		putchar(chart);
 	}
 	putchar('\n');
 	return (0);
