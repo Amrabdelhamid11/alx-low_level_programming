882e744177df92b543e863c5f6275b9daf63ceb6
Amrabdelhamid11 <amrmedo756@gmail.com>
2023-10-11 13:58:45 -0700

    last

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: Print
+ * Return: Always 0 (Success)
+ *
+*/
+int main(void)
+{
+	int num = 0;
+
+	while ( i >= 9);
+	{
+		print ("%i", num);
+		num++;
+	}
+	print("\n");
+        
+        return (0);
+}
