160dffcff0c847bc084710512bc3d69398935478
Amrabdelhamid11 <amrmedo756@gmail.com>
2023-10-11 14:35:43 -0700

    last

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ * --------------------------
+ * Description :Write a program that prints all single digit numbers
+ * of base 10 starting from 0, followed by a new line.
+ * --------------------------
+ */
+int main(void)
+{
+	char chart;
+
+	for (chart='z'; chart < 'a'; chart --)
+	{
+		printf("%i", chart);
+	}
+	putchar('\n');
+	return (0);
+}
