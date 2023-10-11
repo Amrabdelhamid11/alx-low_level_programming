24dff5e351cefa5cd72fe3358315d9a2ff67856f
Amrabdelhamid11 <amrmedo756@gmail.com>
2023-10-11 14:44:08 -0700

    aaaaaaaaa

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -3,17 +3,16 @@
  * main - Entry point
  * Return: Always 0 (Success)
  * --------------------------
- * Description :Write a program that prints all single digit numbers
- * of base 10 starting from 0, followed by a new line.
+ * Description :Write a program that prints all single reverse  alpha  by a new line.
  * --------------------------
  */
 int main(void)
 {
 	char chart;
 
-	for (chart = 'z'; chart <= 'a'; chart--)
+	for (chart = 'z'; chart >= 'a'; chart--)
 	{
-		printf("%i", chart);
+		putchar( chart);
 	}
 	putchar('\n');
 	return (0);
