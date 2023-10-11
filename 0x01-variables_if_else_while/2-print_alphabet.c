eec138994ae9ba6b91d1a973a7d5775a274d5522
Amrabdelhamid11 <amrmedo756@gmail.com>
2023-10-11 12:47:47 -0700

    last

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ * Description: print a to z
+*/
+
+int main(void)
+{
+	char chart = 'a';
+
+	while (chart <= 'z')
+	{
+		putchar(char);
+		chart++;
+	}
+	putchar('\n');
+
+	return (0);
+}
