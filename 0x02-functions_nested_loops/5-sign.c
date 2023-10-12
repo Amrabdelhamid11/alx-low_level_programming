f1a9b5a0b7c94c3e89c0fedc4f10d2ac12b2885b
Amrabdelhamid11 <amrmedo756@gmail.com>
2023-10-11 18:57:29 -0700

    lasast

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _isalpha - prints the sign of a number
+ * @num: ASCii value.
+ * Return: 1 if c is lowercase or Returns 0 otherwise
+ * Write a function that checks for lowercase character.
+ * Prototype: int _islower(int c);
+ */
+
+int _isalpha(int num)
+{
+	if ((num >= 'a' && num <= 'z') || (num >= 'A' && num <= 'Z'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
