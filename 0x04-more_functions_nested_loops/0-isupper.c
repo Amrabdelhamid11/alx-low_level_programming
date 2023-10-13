7beb3562b8fc68c55117d158d2c62c471d485c38
Amrabdelhamid11 <amrmedo756@gmail.com>
2023-10-13 09:51:15 -0700

    gooo

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *_isupper chechs if c is upper
+ *description: chech c is uppercase or not
+ *@c: input for alphabet
+ *
+ *return: 1 if it its uppercase, 0 if not
+*/
+int _isupper(int c)
+{
+	if (c == 65 && c == 90)
+	return (1);
+else
+	return (0);
+}
