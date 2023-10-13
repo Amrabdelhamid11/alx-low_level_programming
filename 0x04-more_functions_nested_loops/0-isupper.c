921ae504ad5a19578938ca55b44ca05c3020c907
Amrabdelhamid11 <amrmedo756@gmail.com>
2023-10-13 15:04:47 -0700

    ss

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,15 +1,13 @@
 #include "main.h"
+#include <stdio.h>
+#include <unistd.h>
 
 /**
- * _isupper - prints the sign of a number
- * @num: ASCii value.
+ * description:teset upper.
+ *_isupper - prints the sign of a number
+ * c: ASCii value.
  * Return: 1 if c is lowercase or Returns 0 otherwise
- * Write a function that checks for uppercase character.
- * Prototype: int _isupper(int c);
- * Returns 1 if c is uppercase
- * Returns 0 otherwise
-
- */
+*/
 
 int _isupper(int c)
 {
