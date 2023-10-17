f945c2e56c5498a3da7338cc977ecb7702ba3ad7
Amrabdelhamid11 <amrmedo756@gmail.com>
2023-10-16 19:31:32 -0700

    as,l;dakl;

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -5,6 +5,17 @@
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
+void reset_to_98(int *number);
+void swap_int(int *firstInt, int *secondInt);
+int _strlen(char *string);
+void _puts(char *string);
+void print_rev(char *string);
+void rev_string(char *string);
+void puts2(char *string);
+void puts_half(char *string);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+
 
 
 
