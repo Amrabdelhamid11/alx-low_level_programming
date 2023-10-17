7962ed3c3f8e9d027cbc5d79e08a19ec0fe5c83b
Amrabdelhamid11 <amrmedo756@gmail.com>
2023-10-16 17:28:22 -0700

    last

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,6 +1,16 @@
 #include "main.h"
 #include <stdio.h>
-
+/**
+ * reset_to_98 - Write a function that takes a
+ * pointer to an int as parameter and updates
+ * the value it points to to 98.
+ * Return: void
+ * @number: number of bytes written
+ * --------------------------
+ * Write a function that takes a
+ * pointer to an int as parameter and updates
+ * the value it points to to 98.
+ */
 void reset_to_98(int *n);
 {
 	*n = 98;
