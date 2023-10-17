69261fa8de9d6195a5b916ce58a072f193fc7889
Amrabdelhamid11 <amrmedo756@gmail.com>
2023-10-16 17:57:37 -0700

    last

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,19 @@
+#include"main.h"
+#include <stdio.h>
+
+/**
+ * swap_int - swap integers value
+ *
+ * @a :first parameter
+ *
+ * @b: second parameter 
+ *
+ * return: nothing
+*/
+int void swap_int(int *a, int *b);
+{
+	int temp;
+       	temp= *a;
+	a* = *b ;
+	*b = temp;
+}
