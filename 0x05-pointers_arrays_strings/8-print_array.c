6523613ad11ad300b75ee2cec47a7e37cc1e56f2
Amrabdelhamid11 <amrmedo756@gmail.com>
2023-10-16 19:45:04 -0700

    sss

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - Write a function that prints n
+ * elements of an array of integers,
+ * followed by a new line.
+ * @array: The array of integers to print
+ * @numberOfElements: The number of elements to print
+ * --------------------------
+ * Write a function that prints half of a string,
+ * followed by a new line.
+ */
+void print_array(int *array, int numberOfElements)
+{
+	int num = 0;
+	/* Use a 'for' loop to iterate through the elements of the array. */
+	for (; num < numberOfElements; num++)
+	{
+		printf("%d", array[num]);
+		/* If it's not the last element, print a comma */
+		/* and space to separate elements. */
+		if (num < numberOfElements - 1)
+		{
+			printf(", ");
+		}
+	}
+	/* Print a new line character to create a newline in the output. */
+	printf("\n");
+}
