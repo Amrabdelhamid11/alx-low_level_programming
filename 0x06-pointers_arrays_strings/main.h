16bf99f51abafab02562af9e2fe012a825f91685
Amrabdelhamid11 <amrmedo756@gmail.com>
2023-10-19 19:03:47 -0700

    task

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,35 @@
+#ifndef MAIIN_H
+#define MAIN_H
+#include <stdio.h>
+
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void swap_int(int *firstInt, int *secondInt);
+int _strlen(char *string);
+void _puts(char *string);
+int _putchar(char character);
+void reset_to_98(int *number);
+void swap_int(int *firstInt, int *secondInt);
+int _strlen(char *string);
+void reset_to_98(int *number);
+void swap_int(int *firstInt, int *secondInt);
+int _strlen(char *string);
+void _puts(char *string);
+void print_rev(char *string);
+void rev_string(char *string);
+void puts2(char *string);
+void puts_half(char *string);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+
+
+
+
+
+
+
+
+
+
+#endif
