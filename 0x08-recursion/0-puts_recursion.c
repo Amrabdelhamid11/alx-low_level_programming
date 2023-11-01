2b4294a600022740916d98ff935c655b7003336e
Amrabdelhamid11 <amrmedo756@gmail.com>
2023-11-01 05:06:26 -0700

    lol

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,23 +1,31 @@
 #include <stdio.h>
 #include "main.h"
+
 /**
  * _puts_recursion - Write a function that
  * prints a string, followed by a new line.
  * Return: Always 0 (Success)
  * --------------------------
  * Prototype: void _puts_recursion(char *s);
- * @s: The string to be printed
+ * @string: The string to be printed
  * FYI: The standard library provides a similar
  * function: puts. Run man puts to learn more.
  * --------------------------
  */
-void _puts_recursion(char *s)
-{
-    if (*s == '\0') {
-   	_putchar('\n');}
-    else {
-        _putchar(*s);
-        _puts_recursion(s + 1);
-    }
-}
 
+void _puts_recursion(char *string)
+{
+	/* if string isn't null, return */
+	if (*string)
+	{
+		/* print the string */
+		_putchar(*string);
+		/* call the function again with the rest of the string */
+		_puts_recursion(string + 1);
+	}
+	/* if string is null, print a new line */
+	if (*string == '\0')
+	{
+		_putchar('\n');
+	}
+}
