8200d9b6a9a922c760b9a507de1ecff83fbcbb87
Amrabdelhamid11 <amrmedo756@gmail.com>
2023-11-07 19:03:22 -0800

    hi

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Write a function that
+ * print the the name of the file of the program was compiled from
+ * followed by a new line
+ * --------------------------
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	/* print the the name of the file of the program was compiled from */
+	/* followed by a new line */
+	printf("%s\n", __FILE__);
+	return (0);
+}
