208cc33cc058606b6f98dda53d4b33a801a1e18d
Amrabdelhamid11 <amrmedo756@gmail.com>
2023-11-13 12:32:59 -0800

    saaaad

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,77 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * op_add - Returns the sum of two integers
+ * Return: The sum of a and b
+ * --------------------
+ * @a: The first integer
+ * @b: The second integer
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - Returns the difference between two integers
+ * Return: The difference of a and b
+ * ------------------
+ * @a: The first integer
+ * @b: The second integer
+ * ------------------
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - Returns the product of two integers
+ * Return: The product of a and b
+ * ------------------
+ * @a: The first integer
+ * @b: The second integer
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - Returns the result of a divided by b
+ * Return: The result of a divided by b
+ * ------------------
+ * @a: The numerator
+ * @b: The denominator
+ * ------------------
+ * By Youssef_Hassane
+ */
+int op_div(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a / b);
+}
+
+/**
+ * op_mod - Returns the remainder of a divided by b
+ * Return: The remainder of a divided by b
+ * ------------------
+ * @a: The numerator
+ * @b: The denominator
+ * ------------------
+ * By Youssef_Hassane
+ */
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a % b);
+}
