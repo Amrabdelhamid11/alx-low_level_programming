9c1bf8adf0ffb50affddd21e1d013ab4e0c86d7b
Amrabdelhamid11 <amrmedo756@gmail.com>
2023-11-21 18:04:58 -0800

    aadvanced

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_listint_safe - prints a listint_t linked list including loops
+ * Return: The number of nodes in the list
+ * exit program with status 98 on failure
+ * --------------------------
+ * Prototype: print_listint_safe(const listint_t *headNode);
+ * --------------------------
+ * @headNode:  pointer to the head of the list.
+ * --------------------------
+ */
+
+size_t print_listint_safe(const listint_t *headNode)
+{
+	size_t idx;
+	size_t nodeCounter = 0;
+	const listint_t *nodeList[1000];
+
+	while (headNode)
+	{
+		for (idx = 0; idx <= nodeCounter; idx++)
+			if (nodeList[idx] == headNode)
+			{
+				printf("-> [%p] %d\n", (void *)headNode, headNode->n);
+				return (nodeCounter);
+			}
+		nodeList[nodeCounter] = headNode;
+		printf("[%p] %d\n", (void *)headNode, headNode->n);
+		headNode = headNode->next;
+		nodeCounter++;
+	}
+	return (nodeCounter);
+}
---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,48 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * free_listint_safe - frees a listint_t list including loops
+ * Return: the size of the list that was free'd.
+ * --------------------------
+ * Prototype: size_t free_listint_safe(listint_t **h);
+ * --------------------------
+ * @headNode: Double pointer to the head of the list
+ * --------------------------
+ */
+
+
+size_t free_listint_safe(listint_t **headNode)
+{
+	size_t freedNodeCount = 0;
+	int addressDifference;
+	listint_t *currentNode;
+
+	if (!headNode || !*headNode)
+		return (0);
+
+	while (*headNode)
+	{
+		addressDifference = *headNode - (*headNode)->next;
+
+		if (addressDifference > 0)
+		{
+			currentNode = (*headNode)->next;
+			free(*headNode);
+			*headNode = currentNode;
+			freedNodeCount++;
+		}
+		else
+		{
+			free(*headNode);
+			*headNode = NULL;
+			freedNodeCount++;
+			break;
+		}
+	}
+
+	*headNode = NULL;
+	return (freedNodeCount);
+}
