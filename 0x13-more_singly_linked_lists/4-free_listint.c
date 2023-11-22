24478ce55d2ef60c712fece0105113531cd95f1b
Amrabdelhamid11 <amrmedo756@gmail.com>
2023-11-21 17:57:42 -0800

    Ammmmr

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+
+/**
+ * free_listint - frees a listint_t list
+ * --------------------------
+ * Prototype: void free_listint(listint_t *head);
+ * --------------------------
+ * @head: pointer to the head of the listint_t list
+ */
+
+void free_listint(listint_t *head)
+{
+	/* Initialize a pointer to traverse the list */
+	listint_t *current;
+	/* Iterate through the list until the end is reached (NULL) */
+	while (head != NULL)
+	{
+		/* Save the current node in a separate pointer */
+		current = head;
+		/* Move the head to the next node in the list */
+		head = head->next;
+		/* Free the memory allocated for the current node */
+		free(current);
+	}
+}
